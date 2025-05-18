b13699a3bcc21ac6609367af8a9f8d3bcf3bc88d
Mohamed Abdelnaser <mohamed@example.com>
2025-05-19 01:22:12 +0300

    Third commit

--- JS/Edit/edit-photo.js
+++ JS/Edit/edit-photo.js
@@ -1,27 +1,29 @@
 async function changePhoto(formData) {
-
-    let response = await fetch('https://pharmalink.runasp.net/api/profile/uploadPhoto', {
-        method: 'PATCH',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        },
-        body: formData
-    })
-    location.reload();
+  let response = await fetch(
+    "https://pharmalink.runasp.net/api/profile/uploadPhoto",
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(formData),
+    }
+  );
+  location.reload();
 }
 
-let fileInput = document.querySelector('#file-upload');
+let fileInput = document.querySelector("#file-upload");
 
-fileInput.addEventListener('change', function () {
-    const data = new FormData();
-    data.append('img', fileInput.files[0]);
-    // changePhoto(data);
-    // let profile = document.querySelector('.user-profile');
-    let header = document.querySelector('header');
-    let loader = document.querySelector('.wrapper')
-    loader.classList.remove('hidden');
-    // profile.classList.add('overlay');
-    header.classList.add('overlay');
-    document.querySelector('body').classList.add('scroll');
-    changePhoto(data);
+fileInput.addEventListener("change", function () {
+  const data = new FormData();
+  data.append("img", fileInput.files[0]);
+  // changePhoto(data);
+  // let profile = document.querySelector('.user-profile');
+  let header = document.querySelector("header");
+  let loader = document.querySelector(".wrapper");
+  loader.classList.remove("hidden");
+  // profile.classList.add('overlay');
+  header.classList.add("overlay");
+  document.querySelector("body").classList.add("scroll");
+  changePhoto(data);
 });
